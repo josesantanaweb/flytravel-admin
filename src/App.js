ba2c148c9000487a48e404fa4c876bbc26fcee2b
Jose Santana <josesantana.web@gmail.com>
2021-04-12 23:18:40 -0400

    Feat: se añadió pagina de reservaciones

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import Payments from './components/Payments/Payments';
+import Reservations from './components/Reservations/Reservations';
 import Profile from './components/Profile/Profile';
 import Passengers from './components/Passengers/Passengers';
 import './App.css';
@@ -12,6 +13,7 @@ const App = () => {
     <Router>
       <Switch>
         <Layout>
+          <Route path="/reservations" component={Reservations}/>
           <Route path="/profile" component={Profile}/>
           <Route path="/passengers" component={Passengers}/>
           <Route path="/payments" component={Payments}/>
